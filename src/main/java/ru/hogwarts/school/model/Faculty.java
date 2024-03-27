12eb12ce714afdfc86441589387ec3fc8e2707aa
MaximovMaxim <maximovmaxim100500@gmail.com>
2024-03-27 18:45:34 +0300

    Ошибки при запуске

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -2,7 +2,8 @@ package ru.hogwarts.school.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
+
 
 import java.util.Objects;
 
