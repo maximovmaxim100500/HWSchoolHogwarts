e5ee5474d1cddcf800f5236638987aae54fa926f
MaximovMaxim <maximovmaxim100500@gmail.com>
2024-04-11 00:35:46 +0300

    FacultyControllerTest готов

--- src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerTest.java
@@ -1,36 +1,91 @@
 package ru.hogwarts.school.controller;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ru.hogwarts.school.model.Faculty;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FacultyControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private FacultyController facultyController;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    void getFacultyInfo() {
+    void contextLoadsFaculty() throws Exception {
+        Assertions.assertThat(facultyController).isNotNull();
     }
 
     @Test
-    void createFaculty() {
+    void getFacultyInfo() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/faculty/1", String.class))
+                .isNotNull();
     }
 
     @Test
-    void editFaculty() {
+    void createFaculty() throws Exception {
+        Faculty faculty = new Faculty();
+        faculty.setName("facultyTest");
+        faculty.setColor("facultyColorTest");
+        Assertions
+                .assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/faculty", faculty, String.class))
+                .isNotNull();
     }
 
     @Test
-    void deleteFaculty() {
+    void editFaculty() throws Exception {
+        Faculty faculty = new Faculty();
+        faculty.setName("facultyTest");
+        faculty.setColor("facultyColorTest");
+        faculty.setId(53L);
+        ResponseEntity<Faculty> responseEntity = restTemplate.exchange("/faculty", HttpMethod.PUT,
+                new HttpEntity<>(faculty), Faculty.class);
+        Assertions.
+                assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Faculty editFaculty = responseEntity.getBody();
+        Assertions.
+                assertThat(editFaculty).isEqualTo(faculty);
     }
 
     @Test
-    void getFacultyByColor() {
+    void deleteFaculty() throws Exception {
+        Long id = 102L;
+        ResponseEntity<Void> responseEntity = restTemplate.exchange("/faculty/{id}",
+                HttpMethod.DELETE, null, Void.class, id);
+        Assertions.
+                assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
-    void findFacultyByNameOrColor() {
+    void getFacultyByColor() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/faculty/facultyByColor?color=red", String.class))
+                .isNotNull();
     }
 
     @Test
-    void getStudentsOfFaculty() {
+    void findFacultyByNameOrColor() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/faculty/facultyByNameOrColor?name=IT", String.class))
+                .isNotNull();
+    }
+
+    @Test
+    void getStudentsOfFaculty() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/faculty/studentsOfFaculty?facultyId=1", String.class))
+                .isNotNull();
     }
 }
