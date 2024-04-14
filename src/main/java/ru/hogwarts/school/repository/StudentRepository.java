c6d985e7ef004e173f7be668cf54b76972012073
MaximovMaxim <maximovmaxim100500@gmail.com>
2024-04-15 00:20:39 +0300

    StudentControllerWebMvcTest готов

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Student;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     // Collection<Student> findByAge(int age);
 
-    Collection<Student> findAllByAgeBetween(Integer minAge, Integer maxAge);
+    List<Student> findAllByAgeBetween(Integer minAge, Integer maxAge);
 }
