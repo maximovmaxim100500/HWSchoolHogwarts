c93b422627d40611872c495a417d886c4f192a9a
MaximovMaxim <maximovmaxim100500@gmail.com>
2024-05-04 02:47:52 +0300

    HW 4.4

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exceptions.RecordNotFoundException;
@@ -18,28 +20,40 @@ public class FacultyService {
         this.facultyRepository = facultyRepository;
     }
 
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Был вызван метод createFaculty");
         return facultyRepository.save(faculty);
     }
 
     public Faculty findFaculty(long id) {
-        return facultyRepository.findById(id).orElseThrow(RecordNotFoundException::new);
+        logger.info("Был вызван метод findFaculty");
+        try {
+            return facultyRepository.findById(id).orElseThrow(RecordNotFoundException::new);
+        } catch (Exception e) {
+            logger.error("Не найден факультет с id = " + id);
+            throw e;
+        }
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        logger.info("Был вызван метод editFaculty");
         return facultyRepository.save(faculty);
     }
 
     public void removeFaculty(long id) {
+        logger.info("Был вызван метод removeFaculty");
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> facultyByColor(String color) {
+        logger.info("Был вызван метод facultyByColor");
         return facultyRepository.findByColorIgnoreCase(color);
     }
 
     public Faculty facultyByName(String color) {
+        logger.info("Был вызван метод facultyByName");
         return facultyRepository.findFacultyByNameIgnoreCase(color);
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,6 +1,8 @@
 package ru.hogwarts.school.service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -15,8 +17,6 @@ import ru.hogwarts.school.repository.StudentRepository;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.Collection;
 import java.util.List;
 
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
@@ -37,72 +37,101 @@ public class StudentService {
         this.avatarRepository = avatarRepository;
     }
 
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
+
     public Student createStudent(Student student) {
+        logger.info("Был вызван метод createStudent");
         return studentRepository.save(student);
     }
 
     public Student findStudent(long id) {
-        return studentRepository.findById(id).orElseThrow(RecordNotFoundException::new);
+        logger.info("Был вызван метод findStudent");
+        try {
+            return studentRepository.findById(id).orElseThrow(RecordNotFoundException::new);
+        } catch (RecordNotFoundException e) {
+            logger.error("Нет студента с id = " + id);
+            throw e;
+        }
     }
 
     public Student editStudent(Student student) {
+        logger.info("Был вызван метод editStudent");
         return studentRepository.save(student);
     }
 
     public void removeStudent(long id) {
+        logger.info("Был вызван метод removeStudent");
         studentRepository.deleteById(id);
     }
 
     public List<Student> studentsByAgeBetween(Integer minAge, Integer maxAge) {
+        logger.info("Был вызван метод studentsByAgeBetween");
         return studentRepository.findAllByAgeBetween(minAge, maxAge);
     }
 
     public Avatar findAvatar(long studentId) {
-        return avatarRepository.findByStudentId(studentId).orElseThrow();
+        logger.info("Был вызван метод findAvatar");
+        try {
+            return avatarRepository.findByStudentId(studentId).orElseThrow();
+        } catch (Exception e) {
+            logger.error("Не найдена аватарка со studentId = " + studentId);
+            throw e;
+        }
     }
 
     public void uploadAvatar(Long studentId, MultipartFile file) throws IOException {
-        Student student = findStudent(studentId);
+        logger.info("Был вызван метод uploadAvatar");
+        try {
+            Student student = findStudent(studentId);
 
-        Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(file.getOriginalFilename()));
-        Files.createDirectories(filePath.getParent());
-        Files.deleteIfExists(filePath);
+            Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(file.getOriginalFilename()));
+            Files.createDirectories(filePath.getParent());
+            Files.deleteIfExists(filePath);
 
-        try (InputStream is = file.getInputStream();
-             OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
-             BufferedInputStream bis = new BufferedInputStream(is, 1024);
-             BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
-        ) {
-            bis.transferTo(bos);
+            try (InputStream is = file.getInputStream();
+                 OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+                 BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                 BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+            ) {
+                bis.transferTo(bos);
+            }
+
+            Avatar avatar = avatarRepository.findByStudentId(studentId).orElseGet(Avatar::new);
+            avatar.setStudent(student);
+            avatar.setFilePath(filePath.toString());
+            avatar.setFileSize(file.getSize());
+            avatar.setMediaType(file.getContentType());
+            avatar.setData(file.getBytes());
+            avatarRepository.save(avatar);
+        } catch (Exception e) {
+            logger.error("Ошибка при загрузке аватара для студента с studentId = " + studentId);
+            throw e;
         }
-
-        Avatar avatar = avatarRepository.findByStudentId(studentId).orElseGet(Avatar::new);
-        avatar.setStudent(student);
-        avatar.setFilePath(filePath.toString());
-        avatar.setFileSize(file.getSize());
-        avatar.setMediaType(file.getContentType());
-        avatar.setData(file.getBytes());
-        avatarRepository.save(avatar);
     }
 
     private String getExtension(String fileName) {
+        logger.info("Был вызван метод getExtension");
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     public List<Avatar> findAll(Integer pageNumber, Integer pageSize) {
+        logger.info("Был вызван метод findAll");
         PageRequest pageRequest = PageRequest.of(pageNumber - 1, pageSize);
         return avatarRepository.findAll(pageRequest).getContent();
     }
 
     public Integer getQuantityAllStudents() {
+        logger.info("Был вызван метод getQuantityAllStudents");
         return studentRepository.getQuantityAllStudents();
     }
 
     public Integer getAverageAgeOfStudents() {
+        logger.info("Был вызван метод getAverageAgeOfStudents");
         return studentRepository.getAverageAgeOfStudents();
     }
 
     public List<Student> getLatestFiveStudents() {
+        logger.info("Был вызван метод getLatestFiveStudents");
         return studentRepository.getLatestFiveStudents();
     }
 }
